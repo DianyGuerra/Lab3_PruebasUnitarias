ac0278ccea401196bf1052c393eb655a5904fa54
Diana Carolina Guerra Coronel <dcguerra2@espe.edu.ec>
2026-06-02 18:13:13 -0500

    pruebas unitarias de multiply y divide

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Calculator } from './calculator';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App  implements OnInit{
+  ngOnInit(): void {
+    let calculator = new Calculator();
+    let result = calculator.multiply(3,4);
+    console.log(result == 12);  //test passed
+    console.log(result != 9); //test passed
+
+    let result2 = calculator.divide(6,2);
+    console.log(result2 == 3);
+    console.log(result2 != 31);
+
+    let result3 = calculator.divide(6,0);
+    console.log(result3 === null); //test passed
+  }
   protected readonly title = signal('Lab3_PruebasUnitarias');
 }
